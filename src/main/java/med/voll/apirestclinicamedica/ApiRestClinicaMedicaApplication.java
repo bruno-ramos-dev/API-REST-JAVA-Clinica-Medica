0fb2dbe57633efe9a85202e56d8f729305bfe101
BrunoRamos88 <88brunoramos@gmail.com>
2023-11-03 20:33:18 -0300

    Criação da estrutura inicial do projeto

---
+++ src/main/java/med/voll/apirestclinicamedica/ApiRestClinicaMedicaApplication.java
@@ -0,0 +1,13 @@
+package med.voll.apirestclinicamedica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestClinicaMedicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestClinicaMedicaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/med/voll/apirestclinicamedica/ApiRestClinicaMedicaApplicationTests.java
@@ -0,0 +1,13 @@
+package med.voll.apirestclinicamedica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestClinicaMedicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
