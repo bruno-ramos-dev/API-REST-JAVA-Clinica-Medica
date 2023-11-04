8554f7a6388754670d73415b1a4f38f1ef888c18
BrunoRamos88 <88brunoramos@gmail.com>
2023-11-03 22:37:04 -0300

    Implementação do endpoint de cadastro de médicos

--- src/main/java/med/voll/apirestclinicamedica/controller/MedicoController.java
+++ src/main/java/med/voll/apirestclinicamedica/controller/MedicoController.java
@@ -1,6 +1,10 @@
 package med.voll.apirestclinicamedica.controller;
 
 import med.voll.apirestclinicamedica.medico.DadosCadastroMedico;
+import med.voll.apirestclinicamedica.medico.Medico;
+import med.voll.apirestclinicamedica.medico.MedicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,8 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/medicos")
 public class MedicoController {
 
+    @Autowired
+    private MedicoRepository repository;
+
     @PostMapping
+    @Transactional
     public void cadastrar(@RequestBody DadosCadastroMedico dados) {
-        System.out.println(dados);
+        repository.save(new Medico(dados));
     }
 }
