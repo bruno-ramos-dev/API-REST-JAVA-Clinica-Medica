46c361b835232d78996f9dc7fb575b0be225ed7d
BrunoRamos88 <88brunoramos@gmail.com>
2023-11-11 23:22:17 -0300

    Criação da classe service agenda de consultas

---
+++ src/main/java/med/voll/apirestclinicamedica/domain/consulta/AgendaDeConsultas.java
@@ -0,0 +1,26 @@
+package med.voll.apirestclinicamedica.domain.consulta;
+
+import med.voll.apirestclinicamedica.domain.medico.MedicoRepository;
+import med.voll.apirestclinicamedica.domain.paciente.PacienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AgendaDeConsultas {
+
+    @Autowired
+    private ConsultaRepository consultaRepository;
+
+    @Autowired
+    private MedicoRepository medicoRepository;
+
+    @Autowired
+    private PacienteRepository pacienteRepository;
+
+    public void agendar(DadosAgendamentoConsulta dados) {
+        var medico = medicoRepository.findById(dados.idMedico()).get();
+        var paciente = pacienteRepository.findById(dados.idPaciente()).get();
+        var consulta = new Consulta(null, medico, paciente, dados.data());
+        consultaRepository.save(consulta);
+    }
+}
