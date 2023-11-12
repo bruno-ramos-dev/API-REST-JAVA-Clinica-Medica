79f2897cc14350c61a6dda44630cf2f50617beae
BrunoRamos88 <88brunoramos@gmail.com>
2023-11-11 23:30:08 -0300

    Criação da classe ValidacaoException para validações de integridade

--- src/main/java/med/voll/apirestclinicamedica/domain/consulta/AgendaDeConsultas.java
+++ src/main/java/med/voll/apirestclinicamedica/domain/consulta/AgendaDeConsultas.java
@@ -1,5 +1,6 @@
 package med.voll.apirestclinicamedica.domain.consulta;
 
+import med.voll.apirestclinicamedica.domain.ValidacaoException;
 import med.voll.apirestclinicamedica.domain.medico.MedicoRepository;
 import med.voll.apirestclinicamedica.domain.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +19,15 @@ public class AgendaDeConsultas {
     private PacienteRepository pacienteRepository;
 
     public void agendar(DadosAgendamentoConsulta dados) {
+
+        if (!pacienteRepository.existsById(dados.idPaciente())) {
+            throw new ValidacaoException("Id do paciente informado não existe!");
+        }
+
+        if (dados.idMedico() != null && !medicoRepository.existsById(dados.idMedico())) {
+            throw new ValidacaoException("Id do médico informado não existe!");
+        }
+
         var medico = medicoRepository.findById(dados.idMedico()).get();
         var paciente = pacienteRepository.findById(dados.idPaciente()).get();
         var consulta = new Consulta(null, medico, paciente, dados.data());
